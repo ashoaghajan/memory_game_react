17b9e0a58ef3b3becbebabd4a856d7507c56fcd1
Ashot Aghajanyan <ashot.aghajanyan@mauriceward.com>
2021-11-05 12:30:53 +0400

    custom hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,9 @@
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import SingleCard from './components/SingleCard';
-
-const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
-];
+import { useApp } from './hooks/useApp';
 
 function App() {
-  const [cards, setCards] = useState<Card[]>([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState<Card|null>(null);
-  const [choiceTwo, setChoiceTwo] = useState<Card|null>(null);
-  const [disabled, setDisabled] = useState(false);
 
-  useEffect(() => {
-    shuffleCards();
-  },[]);
-
-   const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map(card => ({ ...card, id: uuidv4() }));
-      
-    setCards(shuffledCards);
-    setChoiceOne(null);
-    setTurns(0);
-  }
-
-  
-const compareCards = (card1: Card, card2: Card) => {
-  if(card1.src === card2.src){
-    setCards(prev => prev.map(card => {
-      return card.src === card1.src ? { ...card, matched: true } : card;
-    }));
-  }
-} 
-
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setDisabled(false);
-  }
-
-  const handleChoice = (card: Card) => {
-    if(choiceOne){
-      setDisabled(true);
-      setChoiceTwo(card);
-      compareCards(choiceOne, card);
-      setTurns(prev => prev + 1);
-      setTimeout(resetTurn, 1000);
-    }
-    else{
-      setChoiceOne(card);
-    }
-  }
+  const { shuffleCards, cards, handleChoice, disabled, choiceOne, choiceTwo, turns } = useApp();
 
   return (
     <div className="App">
